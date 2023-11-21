4cf332c12e07046b9960b2527df7ccc21d057b01
Eugene Hnatiuk <gnatyuk.e@gmail.com>
2023-11-21 16:45:45 +0200

    minor fix

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log('Hello, world!')
+
+function some() {
+    console.log('This is the first step')
+    return function next() {
+        console.log('This is the second one')
+    }
+    
+}
+
+const closure = some();
+
+closure();
